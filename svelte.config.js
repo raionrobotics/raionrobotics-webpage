4f72c813bbd197668c2f9b8360818a734bb77e75
jemin hwangbo <jemin.hwangbo@gmail.com>
2025-07-28 07:08:53 +0900

    test

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	kit: {
 		adapter: adapter({
-			fallback: '200.html'
+			fallback: '200.html',
+			strict: false
 		})
-		// no trailingSlash here
 	}
 };
