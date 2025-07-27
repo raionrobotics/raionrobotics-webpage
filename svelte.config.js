16f1b1b3809a9df0d224278af8269cdb867aa847
jemin hwangbo <jemin.hwangbo@gmail.com>
2025-07-28 07:22:23 +0900

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
-/** @type {import('@sveltejs/kit').Config} */
-import cloudflare from '@sveltejs/adapter-cloudflare'
+import adapter from '@sveltejs/adapter-cloudflare';
 
-const config = {
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: cloudflare()
+		adapter: adapter({
+			routes: {
+				include: ['/*'], // match all
+				exclude: []      // match nothing else
+			}
+		})
 	}
 };
-
-export default config;
