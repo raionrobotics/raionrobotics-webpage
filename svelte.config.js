5bc71c9de32ab5d762552a2007480be5083ca0ed
jemin hwangbo <jemin.hwangbo@gmail.com>
2025-07-28 06:37:31 +0900

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,25 @@
-/** @type {import('@sveltejs/kit').Config} */
-import cloudflare from '@sveltejs/adapter-cloudflare'
+// /** @type {import('@sveltejs/kit').Config} */
+// import cloudflare from '@sveltejs/adapter-cloudflare'
+//
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: cloudflare()
+// 	}
+// };
+//
+// export default config;
 
-const config = {
+import adapter from '@sveltejs/adapter-static';
+
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: cloudflare()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
-
-export default config;
